56e1584fc2474da372ce0288a79fa22a37f38636
Janin-Michel-Mathias <micheljanin296@gmail.com>
2023-02-10 12:40:52 +0100

    fix indent

--- code_pour_dessiner.asm
+++ code_pour_dessiner.asm
@@ -41,8 +41,9 @@ depth:         	resd	1
 connection:    	resd	1
 width:         	resd	1
 height:        	resd	1
-window:		resq	1
-gc:		resq	1
+window:		    resq	1
+gc:		        resq    1
+coords:         resw    3
 
 section .data
 
@@ -70,170 +71,173 @@ mov     rax,qword[display_name]
 mov     eax,dword[rax+0xe0]
 mov     dword[screen],eax
 
-mov rdi,qword[display_name]
-mov esi,dword[screen]
-call XRootWindow
-mov rbx,rax
+mov     rdi,qword[display_name]
+mov     esi,dword[screen]
+call    XRootWindow
+mov     rbx,rax
 
-mov rdi,qword[display_name]
-mov rsi,rbx
-mov rdx,10
-mov rcx,10
-mov r8,400	; largeur
-mov r9,400	; hauteur
-push 0xFFFFFF	; background  0xRRGGBB
-push 0x00FF00
-push 1
-call XCreateSimpleWindow
-mov qword[window],rax
+mov     rdi,qword[display_name]
+mov     rsi,rbx
+mov     rdx,10
+mov     rcx,10
+mov     r8,400	; largeur
+mov     r9,400	; hauteur
+push    0xFFFFFF	; background  0xRRGGBB
+push    0x00FF00
+push    1
+call    XCreateSimpleWindow
+mov     qword[window],rax
 
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,131077 ;131072
-call XSelectInput
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,131077 ;131072
+call    XSelectInput
 
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-call XMapWindow
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+call    XMapWindow
 
-mov rsi,qword[window]
-mov rdx,0
-mov rcx,0
-call XCreateGC
-mov qword[gc],rax
+mov     rsi,qword[window]
+mov     rdx,0
+mov     rcx,0
+call    XCreateGC
+mov     qword[gc],rax
 
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov rdx,0x000000	; Couleur du crayon
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     rdx,0x000000	; Couleur du crayon
+call    XSetForeground
 
 boucle: ; boucle de gestion des évènements
-mov rdi,qword[display_name]
-mov rsi,event
-call XNextEvent
+mov     rdi,qword[display_name]
+mov     rsi,event
+call    XNextEvent
 
-cmp dword[event],ConfigureNotify	; à l'apparition de la fenêtre
-je dessin							; on saute au label 'dessin'
+cmp     dword[event],ConfigureNotify	; à l'apparition de la fenêtre
+je      dessin							; on saute au label 'dessin'
 
-cmp dword[event],KeyPress			; Si on appuie sur une touche
-je closeDisplay						; on saute au label 'closeDisplay' qui ferme la fenêtre
-jmp boucle
+cmp     dword[event],KeyPress			; Si on appuie sur une touche
+je      closeDisplay						; on saute au label 'closeDisplay' qui ferme la fenêtre
+jmp     boucle
 
-;#########################################
-;#		DEBUT DE LA ZONE DE DESSIN		 #
-;#########################################
-dessin:
+; ;#########################################
+; ;#		DEBUT DE LA ZONE DE DESSIN		 #
+; ;#########################################
+; dessin:
 
 ; couleurs sous forme RRGGBB où RR esr le niveau de rouge, GG le niveua de vert et BB le niveau de bleu
 ; 0000000 (noir) à FFFFFF (blanc)
 
 ;couleur du point 1
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0xFF0000	; Couleur du crayon ; rouge
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0xFF0000	; Couleur du crayon ; rouge
+call    XSetForeground
 
 ; Dessin d'un point rouge : coordonnées (100,200)
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,100	; coordonnée source en x
-mov r8d,200	; coordonnée source en y
-call XDrawPoint
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,100	; coordonnée source en x
+mov     r8d,200	; coordonnée source en y
+call    XDrawPoint
 
 ;couleur du point 2
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0x00FF00	; Couleur du crayon ; vert
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0x00FF00	; Couleur du crayon ; vert
+call    XSetForeground
 
 ; Dessin d'un point vert: coordonnées (100,250)
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,100	; coordonnée source en x
-mov r8d,250	; coordonnée source en y
-call XDrawPoint
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,100	; coordonnée source en x
+mov     r8d,250	; coordonnée source en y
+call    XDrawPoint
 
 ;couleur du point 3
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0x0000FF	; Couleur du crayon ; bleu
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0x0000FF	; Couleur du crayon ; bleu
+call    XSetForeground
 
 ; Dessin d'un point bleu : coordonnées (200,200)
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,200	; coordonnée source en x
-mov r8d,200	; coordonnée source en y
-call XDrawPoint
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,200	; coordonnée source en x
+mov     r8d,200	; coordonnée source en y
+call    XDrawPoint
 
 ;couleur du point 4
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0xFF00FF	; Couleur du crayon ; violet
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0xFF00FF	; Couleur du crayon ; violet
+call    XSetForeground
 
 ; Dessin d'un point violet : coordonnées (200,250)
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,200	; coordonnée source en x
-mov r8d,250	; coordonnée source en y
-call XDrawPoint
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,200	; coordonnée source en x
+mov     r8d,250	; coordonnée source en y
+call    XDrawPoint
 
 ;couleur de la ligne 1
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0x000000	; Couleur du crayon ; noir
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0x000000	; Couleur du crayon ; noir
+call    XSetForeground
 ; coordonnées de la ligne 1 (noire)
-mov dword[x1],50
-mov dword[y1],50
-mov dword[x2],200
-mov dword[y2],350
+mov     dword[x1],50
+mov     dword[y1],50
+mov     dword[x2],200
+mov     dword[y2],350
 ; dessin de la ligne 1
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,dword[x1]	; coordonnée source en x
-mov r8d,dword[y1]	; coordonnée source en y
-mov r9d,dword[x2]	; coordonnée destination en x
-push qword[y2]		; coordonnée destination en y
-call XDrawLine
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,dword[x1]	; coordonnée source en x
+mov     r8d,dword[y1]	; coordonnée source en y
+mov     r9d,dword[x2]	; coordonnée destination en x
+push    qword[y2]		; coordonnée destination en y
+call    XDrawLine
 
 ;couleur de la ligne 2
-mov rdi,qword[display_name]
-mov rsi,qword[gc]
-mov edx,0xFFAA00	; Couleur du crayon ; orange
-call XSetForeground
+mov     rdi,qword[display_name]
+mov     rsi,qword[gc]
+mov     edx,0xFFAA00	; Couleur du crayon ; orange
+call    XSetForeground
 ; coordonnées de la ligne 1 (noire)
-mov dword[x1],300
-mov dword[y1],50
-mov dword[x2],50
-mov dword[y2],350
+mov     dword[x1],300
+mov     dword[y1],50
+mov     dword[x2],50
+mov     dword[y2],350
 ; dessin de la ligne 1
-mov rdi,qword[display_name]
-mov rsi,qword[window]
-mov rdx,qword[gc]
-mov ecx,dword[x1]	; coordonnée source en x
-mov r8d,dword[y1]	; coordonnée source en y
-mov r9d,dword[x2]	; coordonnée destination en x
-push qword[y2]		; coordonnée destination en y
-call XDrawLine
+mov     rdi,qword[display_name]
+mov     rsi,qword[window]
+mov     rdx,qword[gc]
+mov     ecx,dword[x1]	; coordonnée source en x
+mov     r8d,dword[y1]	; coordonnée source en y
+mov     r9d,dword[x2]	; coordonnée destination en x
+push    qword[y2]		; coordonnée destination en y
+call    XDrawLine
 
 
 ; ############################
 ; # FIN DE LA ZONE DE DESSIN #
 ; ############################
+
+
+
 jmp flush
 
 flush:
-mov rdi,qword[display_name]
-call XFlush
-jmp boucle
-mov rax,34
+mov     rdi,qword[display_name]
+call    XFlush
+jmp     boucle
+mov     rax,34
 syscall
 
 closeDisplay:

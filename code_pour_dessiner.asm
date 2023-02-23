cc69f89a1a9cd3b660e72d4647fa5f32a92cff67
Anonm13 <micheljanin296@gmail.com>
2023-02-23 02:53:55 +0100

    fix

--- code_pour_dessiner.asm
+++ code_pour_dessiner.asm
@@ -61,7 +61,7 @@ x1:	dd	0
 x2:	dd	0
 y1:	dd	0
 y2:	dd	0
-print: dd "%d: %d", 10, 0
+print: dd "%d: %d ", 10, 0
 
 section .text
 	
@@ -194,7 +194,7 @@ call    XSetForeground
 
 mov r12, coordsX
 mov r13, coordsY
-; call sensTriangle
+call sensTriangle
 
 ; mov byte[sensTriangleVar], r14b
 
